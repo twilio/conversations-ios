6853e76b8b10cb37c28ae52fee77ab6ed1b9d094
Ilia Kolo <ilya.kolo@gmail.com>
2021-03-29 13:50:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "TwilioChatClient",
+            name: "TwilioConversationsClient",
             url: "https://media.twiliocdn.com/sdk/ios/conversations/releases/1.3.0/twilio-conversations-ios-1.3.0.zip",
             checksum: "49a32dc1a28a1701279f82c2d60878d5b51c3cb31340f23708a2fa217c9e8746"
         )
