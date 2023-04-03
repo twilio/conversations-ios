cab1f93d63e9b6646604172113694fb7e2e8f91e
Distiller <distiller@static.38.23.39.149.cyberlynk.net>
2023-04-03 08:19:27 +0000

    Update Package.swift to 3.0.0

--- Package.swift
+++ Package.swift
@@ -1,21 +1,35 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "TwilioConversationsClient",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "TwilioConversationsClient",
-            targets: ["TwilioConversationsClient"]),
+            targets: ["TwilioConversationsClientTarget"]),
+    ],
+    dependencies: [
+        .package(
+            name: "TwilioTwilsockLib",
+            url: "https://github.com/twilio/twilsock-ios",
+            .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
+        .target(
+          name: "TwilioConversationsClientTarget",
+          dependencies: [
+              .target(name: "TwilioConversationsClient"), 
+              .product(name: "TwilioTwilsockLib", package: "TwilioTwilsockLib")
+          ],
+          path: "Dummy"
+        ),
         .binaryTarget(
             name: "TwilioConversationsClient",
-            url: "https://sdk.twilio.com/ios/conversations/releases/2.2.8/twilio-conversations-ios-2.2.8.zip",
-            checksum: "740d8093d2c70d68be4acc1533e2d2a28f4ffbd445017c2ee83d9027d964b4a0"
-        )
+            url: "https://sdk.twilio.com/ios/conversations/releases/3.0.0/twilio-conversations-ios-3.0.0.zip",
+            checksum: "4daf448d037eb2e3765016d066618d9d43259e297203bb79ee472d36d9519ef3"
+        ),
     ]
 )
